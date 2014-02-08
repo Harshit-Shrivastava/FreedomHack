05735b77aaa710652c6ed6e34842d63123eed848
Saurabh Sood <saurabhsood91@gmail.com>
2014-02-08 16:19:34 +0530

    route distances time and alternatives

--- www/ui/js/maps/maps.js
+++ www/ui/js/maps/maps.js
@@ -1,7 +1,9 @@
 var geocoder;
 var map;
+var directionsService;
 function initialize() {
   geocoder = new google.maps.Geocoder();
+  directionsService = new google.maps.DirectionsService();
   var latlng = new google.maps.LatLng(-34.397, 150.644);
   var mapOptions = {
     zoom: 8,
@@ -20,6 +22,28 @@ function codeAddress(place) {
       });
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function calcRoute(start, end) {
+  // Retrieve the start and end locations and create
+  // a DirectionsRequest using WALKING directions.
+  var request = {
+      origin: start,
+      destination: end,
+      travelMode: google.maps.TravelMode.DRIVING,
+      provideRouteAlternatives: true,
+      avoidHighways: false,
+      avoidTolls: false
+
+  };
+
+  // Route the directions and pass the response to a
+  // function to create markers for each step.
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      return response;
     }
   });
 }
