1a1f4420653ed4966f6ffd35841ddbcf604b7047
Saurabh Sood <saurabhsood91@gmail.com>
2014-02-08 19:25:18 +0530

    annotations and async fix

--- www/ui/js/maps/maps.js
+++ www/ui/js/maps/maps.js
@@ -4,6 +4,7 @@ var directionsService;
 var markerArray;
 function initialize() {
   geocoder = new google.maps.Geocoder();
+  markerArray = [];
   directionsService = new google.maps.DirectionsService();
   var latlng = new google.maps.LatLng(-34.397, 150.644);
   var mapOptions = {
@@ -13,13 +14,19 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-function codeAddress(place) {
+function drawMap(place, contentString) {
   geocoder.geocode( { 'address': place}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
           map: map,
           position: results[0].geometry.location
+      });
+      var infowindow = new google.maps.InfoWindow({
+          content: contentString
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
       });
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
@@ -52,7 +59,40 @@ function calcRoute(start, end) {
   // function to create markers for each step.
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
-      return response;
+      //return response;
+      //showSteps(response, 0);
+      //Show Steps to draw path
     }
+  });
+}
+
+function attachInstructionText(marker, text) {
+  google.maps.event.addListener(marker, 'click', function() {
+    // Open an info window when the marker is clicked on,
+    // containing the text of the step.
+    stepDisplay.setContent(text);
+    stepDisplay.open(map, marker);
+  });
+}
+
+function showSteps(directionResult, index) {
+  var myRoute = directionResult.routes[index].legs[0];
+
+    for (var i = 0; i < myRoute.steps.length; i++) {
+      var marker = new google.maps.Marker({
+        position: myRoute.steps[i].start_location,
+        map: map
+      });
+      attachInstructionText(marker, myRoute.steps[i].instructions);
+      markerArray[i] = marker;
+    }
+}
+
+function attachInstructionText(marker, text) {
+  google.maps.event.addListener(marker, 'click', function() {
+    // Open an info window when the marker is clicked on,
+    // containing the text of the step.
+    stepDisplay.setContent(text);
+    stepDisplay.open(map, marker);
   });
 }
